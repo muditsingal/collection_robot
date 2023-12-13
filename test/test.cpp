c943c52ce037b28661a3c2b9e33a7b6f6f3a3991
armgits <126507842+armgits@users.noreply.github.com>
2023-12-13 00:31:31 -0500

    Backlog 1.3.1 - Initial source file for test suite

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file test.cpp
+ * @author Abhishekh Reddy (areddy@umd.edu)
+ * @author Tommy Chang (chang177@umd.edu)
+ * @brief Test cases for various nodes used in the collection_robot package.
+ * @version 0.5
+ * @date 2023-12-12
+ *
+ * @copyright Copyright (c) 2023 Abhishekh Reddy, Tommy Chang
+ *
+ */
+
+#include <rclcpp/rclcpp.hpp>
+#include <gtest/gtest.h>
+#include <stdlib.h>
