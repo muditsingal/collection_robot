d87ef1fa54ba7d1e0f73ec9aa9b378386d99ccde
abhimanyu <abhi.manyu1225@gmail.com>
2023-12-20 04:06:42 -0500

    Task 2.6.1: update method in ObjectDetect

--- src/object_detector.cpp
+++ src/object_detector.cpp
@@ -1,35 +1,34 @@
 /**
  * @file object_detector.cpp
  * @author Mudit Singal (muditsingal@gmail.com)
+ * @author Abhimanyu Saxena (asaxena4@umd.edu)
  * @brief C++ file for performing detections of blocks and publishing detections
  * @version 0.1
- * @date 2023-12-11
+ * @date 2023-12-19
  *
  * @copyright Copyright (c) 2023
  *
  */
 #include "object_detector.hpp"
+#include <functional>
+#include <rclcpp/logging.hpp>
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+#include <opencv2/core/types.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 using std::placeholders::_1;
 
 ObjectDetect::ObjectDetect() : Node("object_detector") {
-  /**
-   * @brief Publisher object for publishing pose of nearest detected pose
-   *
-   */
-  nearest_object_pose = this->create_publisher<POSE>(
-    "/object_detector/nearest_pose", 10);
 
-  /**
-   * @brief Timer for publishing the messages with frequency of 10 Hz
-   *
-   */
-  timer_ = this->create_wall_timer(
-    100ms, std::bind(&ObjectDetect::nearest_object_pose, this));
-
-  /**
-   * @brief Subscriber object for subscribing to the image published by Camera
-   * onboard the Turtlebot3 Waffle Pi
-   */
-  read_frames_ = this->create_subscription<IAMGE>(
-  "/camera/image_raw", 10, std::bind(&ObjectDetect::framesCallback, this, _1));
+    // Init the pub,sub and image processing pipeline
+    image_node = rclcpp::Node::make_shared("image_listener", node_options);
+    pub_velocity_msg = this->create_publisher<TWIST>("cmd_vel", 10);
+    // Init odom subscriber
+    odom_node = rclcpp::Node::make_shared("odom_node");
+    odom_sub = odom_node->create_subscription<ODOM>("odom", 10,
+        std::bind(&ObjectDetect::odometry_read_callback, this, _1));
+    // init image publisher
+    test_img_pub = this->create_publisher<IMAGE>("/contour_img", 10);
+    trash_detector_pub = this->create_publisher<BOOL_>("/trash_found", 10);
 }
