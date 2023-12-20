7b4c7812993517b7ad8187612d2edefc40ddd1d0
muditsingal <muditsingal@gmail.com>
2023-12-20 04:10:18 -0500

    Task 2.7.1: Added header files and created constructor for handle trash

---
+++ src/handle_trash.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file handle_trash.cpp
+ * @author Mudit Singal (muditsingal@gmail.com)
+ * @brief C++ file for exploring the workspace to detect blocks
+ * @version 0.1
+ * @date 2023-12-11
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "handle_trash.hpp"
+#include <rclcpp/executors.hpp>
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+HandleTrash::HandleTrash() : Node("handle_trash") {
+  unspawn_client = create_client<gazebo_msgs::srv::DeleteEntity>("delete_entity");
+  trash_handler_node = rclcpp::Node::make_shared("handle_trash");
+  block_indices={};
+  blk_cntr = 3;
+  for(int i = 0; i < blk_cntr; i++)
+    block_indices.push_back(i);
+}
